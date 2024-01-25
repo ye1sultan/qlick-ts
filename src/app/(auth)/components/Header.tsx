fd622f2644245dfa140ebbddd0d83e643ea2b7d0
Yels <niyaztaye@gmail.com>
2024-01-25 14:18:12 +0600

    style: Change margin bottom

--- src/app/(auth)/components/Header.tsx
+++ src/app/(auth)/components/Header.tsx
@@ -1,3 +1,3 @@
 export const AuthHeader = ({ text }: { text: string }) => (
-    <h1 className="scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-normal first:mt-0 self-start mb-8">{text}</h1>
+    <h1 className="scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-normal first:mt-0 self-start mb-6">{text}</h1>
 );
