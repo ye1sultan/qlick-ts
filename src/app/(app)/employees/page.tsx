ddaba897996aa31284d0342ac14dfac6b9eac220
Yels <niyaztaye@gmail.com>
2024-01-29 11:16:43 +0600

    ref: Create separate file for search-input

--- src/app/(app)/employees/page.tsx
+++ src/app/(app)/employees/page.tsx
@@ -1,26 +1,12 @@
 "use client";
 
-import { Check, Plus, Refresh, Search, UserNotFound } from "@/components/svgs/Icons";
+import { Plus, Refresh } from "@/components/svgs/Icons";
 import { Button } from "@/components/ui/button";
-import {
-    Command,
-    CommandEmpty,
-    CommandGroup,
-    CommandInput,
-    CommandItem,
-    CommandList,
-    CommandSeparator,
-} from "@/components/ui/command";
-import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "@/components/ui/popover";
 import { Separator } from "@/components/ui/separator";
 import { useState } from "react";
 import { Heading } from "../components/Heading";
 import EmployeeRow from "./Employee";
-import { Badge } from "@/components/ui/badge"
+import { SearchInput } from "./search-input";
 
 interface Employee {
     id: number;
@@ -35,16 +21,6 @@ interface Employee {
     department: string;
     departmentValue: string;
     avatarUrl: string;
-}
-
-interface Department {
-    value: string;
-    label: string;
-}
-
-interface Role {
-    value: string;
-    label: string;
 }
 
 const employees: Employee[] = [
@@ -190,81 +166,14 @@ const employees: Employee[] = [
     }
 ];
 
-const createDepartmentsRolesArrays = (employees: Employee[]): [Department[], Role[]] => {
-    const departments: Department[] = [];
-    const roles: Role[] = [];
-
-    employees.forEach(emp => {
-        if (!departments.some(d => d.value === emp.departmentValue)) {
-            departments.push({ value: emp.departmentValue, label: emp.department });
-        }
-
-        const roleValue = emp.role.replace(/\s+/g, '_').toLowerCase();
-        if (!roles.some(r => r.value === roleValue)) {
-            roles.push({ value: roleValue, label: emp.role });
-        }
-    });
-
-    return [departments, roles];
-};
-
 export default function Employees() {
-    const [open, setOpen] = useState<boolean>(false);
-    const [value, setValue] = useState<string>("");
-
-    const [searchQuery, setSearchQuery] = useState<string>("");
-
     const [selectedEmployees, setSelectedEmployees] = useState<Employee[] | null>(employees);
-
-    const [departments, roles] = createDepartmentsRolesArrays(employees);
-    const [selectedType, setSelectedType] = useState<string>("");
-
-    const handleSearchChange = (newValue: string): void => {
-        setSearchQuery(newValue || '');
-    };
+    const [value, setValue] = useState<string>("");
 
     const handleReset = (): void => {
         setSelectedEmployees(employees);
         setValue("");
     }
-
-    const handleSelectEmployee = (currentValue: string): void => {
-        if (currentValue) {
-            const employee = employees.find(e => e.value === currentValue);
-            setSelectedEmployees(employee ? [employee] : []);
-        } else {
-            setSelectedEmployees(employees);
-        }
-
-        setValue(currentValue === value ? "" : currentValue);
-        setOpen(false);
-        setSelectedType("employee");
-    };
-
-    const handleSelectDepOrRoles = (currentValue: string, type: string): void => {
-        if (currentValue) {
-            const filteredDepartments = employees.filter(e => e.departmentValue === currentValue);
-            const filteredRoles = employees.filter(e => e.roleValue === currentValue);
-
-            setSelectedEmployees([...filteredDepartments, ...filteredRoles])
-        } else {
-            setSelectedEmployees(employees);
-        }
-
-        setValue(currentValue === value ? "" : currentValue);
-        setOpen(false);
-        setSelectedType(type);
-    }
-
-    const filteredEmployees = employees.filter((employee) =>
-        employee.name.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-
-    const filteredDepartmentsAndRoles = [
-        ...departments.map((d) => ({ ...d, type: 'department' })),
-        ...roles.map((r) => ({ ...r, type: 'role' }))
-    ].filter((item) => item.label.toLowerCase().includes(searchQuery.toLowerCase()));
-
 
     return (
         <>
@@ -274,78 +183,13 @@ export default function Employees() {
             </div>
             <div className="w-full flex flex-row justify-start items-center gap-y-2 sm:pr-4">
                 <div className="w-full flex flex-col sm:flex-row justify-center items-start sm:justify-start sm:items-center gap-2">
-                    <Popover open={open} onOpenChange={setOpen}>
-                        <PopoverTrigger asChild>
-                            <Button
-                                variant="outline"
-                                role="combobox"
-                                aria-expanded={open}
-                                className="max-w-[300px] w-full flex justify-start items-center gap-x-2"
-                            >
-                                <Search className="stroke-zinc-500 h-4 w-4" />
-                                {value ? (
-                                    selectedType === 'employee'
-                                        ? selectedEmployees?.find((item) => item.value === value)?.label
-                                        : selectedType === 'department'
-                                            ? departments.find((item) => item.value === value)?.label
-                                            : roles.find((item) => item.value === value)?.label
-                                ) : (
-                                    <span className="text-zinc-500">Search...</span>
-                                )}
-                            </Button>
-                        </PopoverTrigger>
-                        <PopoverContent className="p-0">
-                            <Command
-                                className="rounded-lg border shadow-md"
-                            >
-                                <CommandInput
-                                    placeholder="Search an employee or department..."
-                                    value={searchQuery}
-                                    onValueChange={handleSearchChange}
-                                />
-                                <CommandList>
-                                    <CommandEmpty>
-                                        <div className="flex justify-center items-center gap-x-2">
-                                            <UserNotFound className="stroke-zinc-600 h-5 w-5" />
-                                            No results found.
-                                        </div>
-                                    </CommandEmpty>
-                                    {filteredEmployees.length > 0 && (
-                                        <CommandGroup heading="Employees">
-                                            {filteredEmployees.slice(0, 3).map((employee) => (
-                                                <CommandItem
-                                                    key={employee.id}
-                                                    value={employee.value}
-                                                    className="cursor-pointer"
-                                                    onSelect={(currentValue) => handleSelectEmployee(currentValue)}
-                                                >
-                                                    {employee.name}
-                                                </CommandItem>
-                                            ))}
-                                        </CommandGroup>
-                                    )}
-
-                                    <CommandSeparator />
-
-                                    {filteredDepartmentsAndRoles.length > 0 && (
-                                        <CommandGroup heading="Departments & Roles">
-                                            {filteredDepartmentsAndRoles.slice(0, 3).map((items) => (
-                                                <CommandItem
-                                                    key={items.value}
-                                                    value={items.value}
-                                                    className="cursor-pointer flex justify-between items-center w-full"
-                                                    onSelect={() => handleSelectDepOrRoles(items.value, items.type)}
-                                                >
-                                                    {items.label}
-                                                    <Badge variant="outline" className="font-medium">{items.type}</Badge>
-                                                </CommandItem>
-                                            ))}
-                                        </CommandGroup>
-                                    )}
-                                </CommandList>
-                            </Command>
-                        </PopoverContent>
-                    </Popover>
+                    <SearchInput
+                        employees={employees}
+                        value={value}
+                        setValue={setValue}
+                        selectedEmployees={selectedEmployees}
+                        setSelectedEmployees={setSelectedEmployees}
+                    />
                     <div className="w-full flex justify-start sm:justify-end items-center gap-2">
                         <Button size="icon" className="bg-indigo-400 hover:bg-indigo-500">
                             <Plus className="stroke-zinc-50" />
@@ -379,13 +223,6 @@ export default function Employees() {
                     <Separator className="bg-indigo-200" />
                     {selectedEmployees?.map((employee, index) => (
                         <EmployeeRow key={index} employee={employee} />
-                    ))}
-                </div>
-                <div className="md:hidden block border-[1px] border-indigo-200 rounded-md my-4">
-                    {selectedEmployees?.map((employee, index) => (
-                        <>
-                            d
-                        </>
                     ))}
                 </div>
             </div>
