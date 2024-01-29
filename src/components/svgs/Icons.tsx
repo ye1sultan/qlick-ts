efe7166488a0a811bb9d302ebe5f9eda7c31f27d
Yels <niyaztaye@gmail.com>
2024-01-29 11:10:13 +0600

    fix: stroke-linejoin to strokeLinejoin

--- src/components/svgs/Icons.tsx
+++ src/components/svgs/Icons.tsx
@@ -164,6 +164,6 @@ export const UserAvatar = (props: SvgProps) => (
 
 export const Edit = (props: SvgProps) => (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-        <path d="M18 10L14 6M18 10L21 7L17 3L14 6M18 10L17 11M14 6L8 12V16H12L14.5 13.5M20 14V20H12M10 4L4 4L4 20H7" strokeWidth="2" strokeLinecap="round" stroke-linejoin="round" />
+        <path d="M18 10L14 6M18 10L21 7L17 3L14 6M18 10L17 11M14 6L8 12V16H12L14.5 13.5M20 14V20H12M10 4L4 4L4 20H7" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
 );
