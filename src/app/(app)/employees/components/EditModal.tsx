f68884e1358e6796b23f663701a2484b3d8a26e3
ye1sultan <niyaztaye@gmail.com>
2024-02-23 14:33:39 +0600

    ref: refactor EditModal.tsx

--- src/app/(app)/employees/components/EditModal.tsx
+++ src/app/(app)/employees/components/EditModal.tsx
@@ -1,6 +1,5 @@
 import { Edit as EditIcon } from '@/components/svgs/Icons';
 import { Button } from '@/components/ui/button';
-import { Calendar } from '@/components/ui/calendar';
 import {
 	Dialog,
 	DialogContent,
@@ -12,37 +11,19 @@ import {
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import {
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
-} from '@/components/ui/popover';
-import { CalendarIcon } from '@radix-ui/react-icons';
 import { useRef } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import type { Employee } from '../types/IEmployee';
-import { formatDate } from '../utils/formatDate';
 
 export const Edit = ({
 	employee,
-	open,
-	setOpen,
-	date,
-	setDate,
 }: {
 	employee: Employee;
-	open: boolean;
-	setOpen: (open: boolean) => void;
-	date: Date;
-	setDate: (date: Date) => void;
 }) => {
 	const close = useRef<HTMLDivElement>(null);
 
 	const {
-		register,
-		handleSubmit,
-		setValue,
-		formState: { errors },
+		register
 	} = useForm<Employee>({ defaultValues: employee });
 
 	const onSubmit: SubmitHandler<Employee> = (data) => {
@@ -126,42 +107,6 @@ export const Edit = ({
 							className="col-span-3"
 							type="tex"
 						/>
-					</div>
-
-					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="joinDate" className="text-right">
-							Join Date
-						</Label>
-
-						<Popover open={open}>
-							<PopoverTrigger asChild>
-								<Button
-									onClick={() => setOpen(true)}
-									variant="outline"
-									className="w-[240px] pl-3 text-left font-normal"
-								>
-									{date ? (
-										formatDate(date.toISOString())
-									) : (
-										<span>Select a date</span>
-									)}
-									<CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-								</Button>
-							</PopoverTrigger>
-							<PopoverContent className="w-auto p-0" align="center">
-								<Calendar
-									mode="single"
-									selected={date || undefined}
-									onSelect={(newDate) => (
-										setDate(newDate || new Date()), setOpen(false)
-									)}
-									disabled={(date) =>
-										date > new Date() || date < new Date('1900-01-01')
-									}
-									initialFocus
-								/>
-							</PopoverContent>
-						</Popover>
 					</div>
 				</div>
 				<DialogFooter>
