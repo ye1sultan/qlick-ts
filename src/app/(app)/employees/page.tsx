c82c5a8f977bb702b61a19ce958b1c9a46fd16cd
ye1sultan <niyaztaye@gmail.com>
2024-02-23 14:36:30 +0600

    ref: refactor page

--- src/app/(app)/employees/page.tsx
+++ src/app/(app)/employees/page.tsx
@@ -1,27 +1,10 @@
 'use client';
 
-import { Plus, Refresh } from '@/components/svgs/Icons';
-import { Button } from '@/components/ui/button';
-import { Separator } from '@/components/ui/separator';
 import { useState } from 'react';
 import { Heading } from '../components/Heading';
-import EmployeeRow from './Employee';
-import { SearchInput } from './search-input';
-
-interface Employee {
-	id: number;
-	name: string;
-	value: string;
-	label: string;
-	email: string;
-	phone: string;
-	role: string;
-	roleValue: string;
-	joinDate: string;
-	department: string;
-	departmentValue: string;
-	avatarUrl: string;
-}
+import { Menu } from './menu';
+import { Table } from './table';
+import { Employee } from './types/IEmployee';
 
 const employees: Employee[] = [
 	{
@@ -29,11 +12,10 @@ const employees: Employee[] = [
 		name: 'John Doe',
 		value: 'john_doe',
 		label: 'John Doe',
-		email: 'john.doe@example.com',
+		email: '@john.doe',
 		phone: '123-456-7890',
 		role: 'Manager',
 		roleValue: 'manager',
-		joinDate: '2020-01-15',
 		department: 'Marketing',
 		departmentValue: 'marketing',
 		avatarUrl: 'https://picsum.photos/1920/1080',
@@ -43,11 +25,10 @@ const employees: Employee[] = [
 		name: 'Jane Smith',
 		value: 'jane_smith',
 		label: 'Jane Smith',
-		email: 'jane.smith@example.com',
+		email: '@jane.smith',
 		phone: '234-567-8901',
 		role: 'Developer',
 		roleValue: 'developer',
-		joinDate: '2019-06-10',
 		department: 'Engineering',
 		departmentValue: 'engineering',
 		avatarUrl: 'https://picsum.photos/1920/1080',
@@ -57,11 +38,10 @@ const employees: Employee[] = [
 		name: 'Emily Johnson',
 		value: 'emily_johnson',
 		label: 'Emily Johnson',
-		email: 'emily.johnson@example.com',
+		email: '@emily.johnson',
 		phone: '345-678-9012',
 		role: 'Designer',
 		roleValue: 'designer',
-		joinDate: '2021-03-20',
 		department: 'Design',
 		departmentValue: 'design',
 		avatarUrl: 'https://picsum.photos/1920/1080',
@@ -71,11 +51,10 @@ const employees: Employee[] = [
 		name: 'Michael Brown',
 		value: 'michael_brown',
 		label: 'Michael Brown',
-		email: 'michael.brown@example.com',
+		email: '@michael.brown',
 		phone: '456-789-0123',
 		role: 'HR Manager',
 		roleValue: 'hr_manager',
-		joinDate: '2018-09-05',
 		department: 'Sales',
 		departmentValue: 'sales',
 		avatarUrl: 'https://picsum.photos/1920/1080',
@@ -85,11 +64,10 @@ const employees: Employee[] = [
 		name: 'Sarah Wilson',
 		value: 'sarah_wilson',
 		label: 'Sarah Wilson',
-		email: 'sarah.wilson@example.com',
+		email: '@sarah.wilson',
 		phone: '567-890-1234',
 		role: 'Recourcer',
 		roleValue: 'recourcer',
-		joinDate: '2019-11-22',
 		department: 'Human Resources',
 		departmentValue: 'human_resources',
 		avatarUrl: 'https://picsum.photos/1920/1080',
@@ -99,11 +77,10 @@ const employees: Employee[] = [
 		name: 'David Lee',
 		value: 'david_lee',
 		label: 'David Lee',
-		email: 'david.lee@example.com',
+		email: '@david.lee',
 		phone: '678-901-2345',
 		role: 'Product Manager',
 		roleValue: 'product_manager',
-		joinDate: '2020-02-19',
 		department: 'Product',
 		departmentValue: 'product',
 		avatarUrl: 'https://picsum.photos/1920/1080',
@@ -113,11 +90,10 @@ const employees: Employee[] = [
 		name: 'Linda Garcia',
 		value: 'linda_garcia',
 		label: 'Linda Garcia',
-		email: 'linda.garcia@example.com',
+		email: '@linda.garcia',
 		phone: '789-012-3456',
 		role: 'Quality Analyst',
 		roleValue: 'quality_analyst',
-		joinDate: '2021-07-30',
 		department: 'Quality Assurance',
 		departmentValue: 'quality_assurance',
 		avatarUrl: 'https://picsum.photos/1920/1080',
@@ -127,11 +103,10 @@ const employees: Employee[] = [
 		name: 'Kevin Martinez',
 		value: 'kevin_martinez',
 		label: 'Kevin Martinez',
-		email: 'kevin.martinez@example.com',
+		email: '@kevin.martinez',
 		phone: '890-123-4567',
 		role: 'Network Administrator',
 		roleValue: 'network_administrator',
-		joinDate: '2018-12-14',
 		department: 'IT',
 		departmentValue: 'it',
 		avatarUrl: 'https://picsum.photos/1920/1080',
@@ -141,11 +116,10 @@ const employees: Employee[] = [
 		name: 'Elizabeth Gonzalez',
 		value: 'elizabeth_gonzalez',
 		label: 'Elizabeth Gonzalez',
-		email: 'elizabeth.gonzalez@example.com',
+		email: '@elizabeth.gonzalez',
 		phone: '901-234-5678',
 		role: 'Accountant',
 		roleValue: 'accountant',
-		joinDate: '2020-05-05',
 		department: 'Finance',
 		departmentValue: 'finance',
 		avatarUrl: 'https://picsum.photos/1920/1080',
@@ -155,11 +129,10 @@ const employees: Employee[] = [
 		name: 'Daniel Harris',
 		value: 'daniel_harris',
 		label: 'Daniel Harris',
-		email: 'daniel.harris@example.com',
+		email: '@daniel.harris',
 		phone: '012-345-6789',
 		role: 'Support Specialist',
 		roleValue: 'support_specialist',
-		joinDate: '2021-09-20',
 		department: 'Customer Service',
 		departmentValue: 'customer_service',
 		avatarUrl: 'https://picsum.photos/1920/1080',
@@ -167,7 +140,7 @@ const employees: Employee[] = [
 ];
 
 export default function Employees() {
-	const [selectedEmployees, setSelectedEmployees] = useState<Employee[] | null>(
+	const [selectedEmployees, setSelectedEmployees] = useState<Employee[] | []>(
 		employees
 	);
 	const [value, setValue] = useState<string>('');
@@ -179,61 +152,19 @@ export default function Employees() {
 
 	return (
 		<>
-			<div className="block space-y-6 pb-4">
-				<Heading
-					heading={'Employees'}
-					subHeading={'Management and analysis of employee information.'}
-				/>
-				<Separator />
-			</div>
-			<div className="flex w-full flex-row items-center justify-start gap-y-2 sm:pr-4">
-				<div className="flex w-full flex-col items-start justify-center gap-2 sm:flex-row sm:items-center sm:justify-start">
-					<SearchInput
-						employees={employees}
-						value={value}
-						setValue={setValue}
-						selectedEmployees={selectedEmployees}
-						setSelectedEmployees={setSelectedEmployees}
-					/>
-					<div className="flex w-full items-center justify-start gap-2 sm:justify-end">
-						<Button size="icon" className="bg-indigo-400 hover:bg-indigo-500">
-							<Plus className="stroke-zinc-50" />
-						</Button>
-						<Button
-							size="icon"
-							variant="outline"
-							className="hover:bg-gray-200"
-							onClick={handleReset}
-						>
-							<Refresh className="h-4 w-4 stroke-zinc-800" />
-						</Button>
-					</div>
-				</div>
-			</div>
-			<div className="h-full w-full py-6">
-				<h1 className="flex items-center justify-start gap-x-2 text-xl font-medium">
-					<span data-testid="employee-count" className="text-indigo-600">
-						{selectedEmployees?.length}
-					</span>
-					<span data-testid="employee-heading">Employees</span>
-				</h1>
-				<div className="my-4 hidden rounded-md border-[1px] border-indigo-200 md:block">
-					<div className="flex w-full items-center justify-between px-8 py-2 text-sm capitalize text-zinc-500">
-						<div className="w-1/4 text-start xl:w-1/5">name</div>
-						<div className="w-1/4 text-start">email</div>
-						<div className="hidden w-1/5 text-start lg:block xl:w-1/6">
-							phone
-						</div>
-						<div className="w-1/5 text-start xl:w-1/6">role</div>
-						<div className="hidden w-1/6 text-start xl:block">join date</div>
-						<div className="w-1/5 text-start xl:w-1/6">department</div>
-					</div>
-					<Separator className="bg-indigo-200" />
-					{selectedEmployees?.map((employee, index) => (
-						<EmployeeRow key={index} employee={employee} />
-					))}
-				</div>
-			</div>
+			<Heading
+				heading={'Employees'}
+				subHeading={'Management and analysis of employee information.'}
+			/>
+			<Menu
+				selectedEmployees={selectedEmployees}
+				setSelectedEmployees={setSelectedEmployees}
+				value={value}
+				setValue={setValue}
+				employees={employees}
+				handleReset={handleReset}
+			/>
+			<Table selectedEmployees={selectedEmployees} />
 		</>
 	);
 }
